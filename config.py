cb928e0e7d6dc8643919a0f795912202f71d5312
Aliaksandr Chaiko <xander.blr@gmail.com>
2016-02-23 21:33:23 +0300

    added sqlalchemy pool recycle option

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ db_user = os.getenv('DB_USER', 'root')
 db_pwd = os.getenv('DB_PWD', '')
 SQLALCHEMY_DATABASE_URI = "mysql://{:s}:{:s}@{:s}:3306/kicker_db".format(db_user, db_pwd, db_host)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_POOL_RECYCLE = 3600
+SQLALCHEMY_POOL_TIMEOUT = 20
 
 # App settings
 APPLICATION_ROOT = '/api'
